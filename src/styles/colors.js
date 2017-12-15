d50dc2b0b5980f6f6c3c9cbe6820c3f9104bde07
Raphael Morales <raphael.morales@outlook.com>
2017-12-15 10:54:45 -0200

    infos

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -6,4 +6,16 @@ export default {
   medium: '#999999',
   dark: '#666666',
   darker: '#333333',
+  /* Escala Preto & Branco */
+  pb_0: '#ffffff',
+  pb_10: '#e5e5e5',
+  pb_20: '#cccccc',
+  pb_30: '#b3b3b3',
+  pb_40: '#999999',
+  pb_50: '#808080',
+  pb_60: '#666666',
+  pb_70: '#4d4d4d',
+  pb_80: '#333333',
+  pb_90: '#1a1a1a',
+  pb_100: '#000000',
 };
